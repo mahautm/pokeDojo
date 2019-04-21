2334b9b7abbfee955be1b52e46933ed59e159d5d
Mahaut <mmahaut@ensc.fr>
2019-04-21 14:55:54 +0200

    Prépapration de la classe joueur.

--- PokeDojo_GGMM/PokeDojo_GGMM/Joueur.cs
+++ PokeDojo_GGMM/PokeDojo_GGMM/Joueur.cs
@@ -8,5 +8,29 @@ namespace PokeDojo_GGMM
 {
     class Joueur
     {
+        public string Nom { get; set; }
+
+        //Un joueur peut avoir jusqu'à trois pokémon dans son sac.
+        //!! Y a t'il une méthode où je n'ai pas besoin de la variable _sac et où je peux tout faire à la propriété?
+        private List<Pokemon> _sac;
+        public List<Pokemon> Sac
+        {
+            get
+            {
+                return this._sac;
+            }
+            set
+            {
+                if(value.Count == 3)
+                this._sac = value;
+            }
+        }
+
+        //Constructeur
+        public Joueur(string nom, List<Pokemon> sac)
+        {
+            _sac = sac;
+            nom = Nom;
+        }
     }
 }
--- PokeDojo_GGMM/PokeDojo_GGMM/Pokemon.cs
+++ PokeDojo_GGMM/PokeDojo_GGMM/Pokemon.cs
@@ -11,23 +11,30 @@ namespace PokeDojo_GGMM
         //Caractéristiques initiales du pokémon.
         //!!Les niveau d'autorisation de lecture et d'écriture sont encore sujets à évoluer.
         public string Nom { get; private set; }
+        //PV pour points de vie
         public int PV { get; private set; }
-        public int PuissanceAttaque { get; set; }
+        //PA pour Puissance d'attaque
+        public int PA { get; private set; }
+        public int Degats { get; set; }
 
         //!!Est-ce qu'on a un nombre limité de types ? l'ennoncé note "etc." ce qui me fait supposer que non.
         //!!Rappel de quelques types : plante, feu, eau, électrique, psy, poison, etc.
-        public string Type { get;}
+        public string TypeElementaire { get;}
 
         //!!Je pense qu'une vérification s'impose, peut être même au sein d'une classe Type
         //!!Que l'on définirait. Il faut que le feu n'ai qu'une seule faiblesse assignée, pour
         //!! Empêcher deux pokémons de type feu d'avoir deux faiblesses différentes. Je pense à un tableau dans une classe.
-        public string Faiblesse { get;}
+        public string TypeFaiblesse { get;}
 
 
         //Constructeur
-        public Pokemon()
+        public Pokemon(string nom, int pV, int pA, string typeElementaire, string typeFaiblesse)
         {
-
+            Nom = nom;
+            PV = pV;
+            PA = pA;
+            TypeElementaire = typeElementaire;
+            TypeFaiblesse = TypeFaiblesse;
         }
     }
 }
