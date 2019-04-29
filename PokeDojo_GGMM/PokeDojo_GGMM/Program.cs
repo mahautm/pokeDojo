cbe05547a8b33f1b90756421316fbcbd1f1c01fa
Guillaume <ggrosse@ensc.fr>
2019-04-29 15:59:55 +0200

    Update Program.cs

--- PokeDojo_GGMM/PokeDojo_GGMM/Program.cs
+++ PokeDojo_GGMM/PokeDojo_GGMM/Program.cs
@@ -14,9 +14,41 @@ namespace PokeDojo_GGMM
             //!! supprimés avant le rendu final avec 2 points d'exclammation
             //!! C'est pour m'aider à coder, pas pour expliquer le code.
 
-            Arene arene = new Arene();
+            //!! LIENS POUR LES POKEMONS
+            //!! https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_base_stats_(Generation_VII-present)
+            //!! https://www.pokebip.com/pokedex/4eme_generation_pokeliste_liste_des_pokemon.html
+            Pokemon p0 = new Pokemon("Bulbizarre", 45, 49, 'P');
+            Pokemon p1 = new Pokemon("Salamèche", 39, 52, 'F');
+            Pokemon p2 = new Pokemon("Carapuce", 44, 48, 'E');
+            Pokemon p3 = new Pokemon("Givrali", 65, 60, 'G');
+            Pokemon p4 = new Pokemon("Minidraco", 41, 64, 'D');
+            Pokemon p5 = new Pokemon("Farfuret", 55, 95, 'T');
+            Pokemon p6 = new Pokemon("Foretress", 75, 90, 'A');
+            Pokemon p7 = new Pokemon("Embrylex", 50, 64, 'R');
+
+            Joueur j0 = new Joueur("Guiguite38", new List<Pokemon> { p0, p1, p2 });
+            Joueur j1 = new Joueur("Matmut14", new List<Pokemon> { p1, p2, p3 });
+            Joueur j2 = new Joueur("Zgoogo33", new List<Pokemon> { p2, p3, p4 });
+            Joueur j3 = new Joueur("BenDlaRochèl", new List<Pokemon> { p3, p4, p5 });
+
+            Arene arene = new Arene(new List<Joueur> { j0, j1, j2, j3 });
         }
 
-        public static void DeroulerPartie()
+        public static void DeroulerPartie(Arene arene)
+        {
+            ;
+        }
+
+        public static void JouerCombat(Arene arene)
+        {
+            ;
+        }
+
+        //deux participants, initiative  = 0 ou 1, désigne le joueur qui commence
+        public static void JouerTour(Arene arene, List<Joueur> participants, int initiative)
+        {
+            ;
+        }
+
     }
 }
